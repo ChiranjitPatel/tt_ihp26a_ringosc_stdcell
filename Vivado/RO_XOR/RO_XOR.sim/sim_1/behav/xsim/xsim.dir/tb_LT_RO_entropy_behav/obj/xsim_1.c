85e707b68df2c800ba1e512dc55440689c3268fc
chiranjitpatel <chiranjitpatel08@gmail.com>
2026-03-22 23:21:43 +0530

    update to LT RO

--- Vivado/RO_XOR/RO_XOR.sim/sim_1/behav/xsim/xsim.dir/tb_LT_RO_entropy_behav/obj/xsim_1.c
+++ Vivado/RO_XOR/RO_XOR.sim/sim_1/behav/xsim/xsim.dir/tb_LT_RO_entropy_behav/obj/xsim_1.c
@@ -72,6 +72,12 @@ IKI_DLLESPEC extern void execute_61(char*, char *);
 IKI_DLLESPEC extern void execute_62(char*, char *);
 IKI_DLLESPEC extern void execute_68(char*, char *);
 IKI_DLLESPEC extern void execute_63(char*, char *);
+IKI_DLLESPEC extern void execute_69(char*, char *);
+IKI_DLLESPEC extern void execute_70(char*, char *);
+IKI_DLLESPEC extern void execute_78(char*, char *);
+IKI_DLLESPEC extern void execute_79(char*, char *);
+IKI_DLLESPEC extern void execute_80(char*, char *);
+IKI_DLLESPEC extern void execute_84(char*, char *);
 IKI_DLLESPEC extern void execute_49(char*, char *);
 IKI_DLLESPEC extern void execute_93(char*, char *);
 IKI_DLLESPEC extern void execute_57(char*, char *);
@@ -85,12 +91,12 @@ IKI_DLLESPEC extern void execute_105(char*, char *);
 IKI_DLLESPEC extern void execute_106(char*, char *);
 IKI_DLLESPEC extern void execute_107(char*, char *);
 IKI_DLLESPEC extern void vlog_transfunc_eventcallback(char*, char*, unsigned, unsigned, unsigned, char *);
-funcp funcTab[31] = {(funcp)execute_50, (funcp)execute_51, (funcp)execute_52, (funcp)execute_53, (funcp)execute_54, (funcp)execute_55, (funcp)execute_97, (funcp)execute_98, (funcp)execute_99, (funcp)execute_100, (funcp)execute_101, (funcp)execute_94, (funcp)execute_95, (funcp)execute_96, (funcp)execute_61, (funcp)execute_62, (funcp)execute_68, (funcp)execute_63, (funcp)execute_49, (funcp)execute_93, (funcp)execute_57, (funcp)execute_58, (funcp)execute_59, (funcp)execute_60, (funcp)execute_102, (funcp)execute_103, (funcp)execute_104, (funcp)execute_105, (funcp)execute_106, (funcp)execute_107, (funcp)vlog_transfunc_eventcallback};
-const int NumRelocateId= 31;
+funcp funcTab[37] = {(funcp)execute_50, (funcp)execute_51, (funcp)execute_52, (funcp)execute_53, (funcp)execute_54, (funcp)execute_55, (funcp)execute_97, (funcp)execute_98, (funcp)execute_99, (funcp)execute_100, (funcp)execute_101, (funcp)execute_94, (funcp)execute_95, (funcp)execute_96, (funcp)execute_61, (funcp)execute_62, (funcp)execute_68, (funcp)execute_63, (funcp)execute_69, (funcp)execute_70, (funcp)execute_78, (funcp)execute_79, (funcp)execute_80, (funcp)execute_84, (funcp)execute_49, (funcp)execute_93, (funcp)execute_57, (funcp)execute_58, (funcp)execute_59, (funcp)execute_60, (funcp)execute_102, (funcp)execute_103, (funcp)execute_104, (funcp)execute_105, (funcp)execute_106, (funcp)execute_107, (funcp)vlog_transfunc_eventcallback};
+const int NumRelocateId= 37;
 
 void relocate(char *dp)
 {
-	iki_relocate(dp, "xsim.dir/tb_LT_RO_entropy_behav/xsim.reloc",  (void **)funcTab, 31);
+	iki_relocate(dp, "xsim.dir/tb_LT_RO_entropy_behav/xsim.reloc",  (void **)funcTab, 37);
 
 	/*Populate the transaction function pointer field in the whole net structure */
 }
